425a5f4a50ad4eafc414518acaaafa6f77008d1c
sungDO639 <sung16101993@gmail.com>
2026-07-02 23:50:53 +0700

    Create ProjectManager.ts

---
+++ packages/project-manager/src/ProjectManager.ts
@@ -0,0 +1,9 @@
+```typescript
+export class ProjectManager {
+  async createProject(name: string, description: string, type: string) {
+    return { id: '1', name, description, type };
+  }
+}
+```
+
+---
